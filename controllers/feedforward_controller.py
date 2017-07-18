c4a84ec0ad476a09ecc80d63811db452217ffe77
garimella.gowtham74@gmail.com <garimella.gowtham74@gmail.com>
2017-07-18 17:58:38 -0400

    Add stochastic option to feedforward controller

--- controllers/feedforward_controller.py
+++ controllers/feedforward_controller.py
@@ -4,15 +4,20 @@ from optimal_control_framework.controllers import AbstractController
 import numpy as np
 
 class FeedforwardController(AbstractController):
-  def __init__(self, us):
+  def __init__(self, us, Sigmaus=None):
     """
     Constructor that stores dynamics
     Parameters:
-    Ks       -- Feedback gains as a [Nxnxm] matrix
-    ks       -- Feedforward controls as [Nxm] matrix
+    us       -- Feedforward controls as [Nxm] matrix
+    Sigmaus  -- Covariance of the controls
     """
     self.us = us
     self.discrete = True
+    self.Sigmaus = Sigmaus
+    if len(us.shape) == 1:
+        self.m = 1
+    else:
+        self.m = us.shape[1]
 
   def deterministic_control(self, i, x):
     """
@@ -23,3 +28,11 @@ class FeedforwardController(AbstractController):
     x   -- Current state
     """
     return self.us[i]
+
+  def control(self, i, x):
+    if self.Sigmaus is None:
+      noise = 0
+    else:
+      noise = np.random.multivariate_normal(np.zeros(self.m),
+                                            np.diag(self.Sigmaus[i]))
+    return self.deterministic_control(i,x) + noise
