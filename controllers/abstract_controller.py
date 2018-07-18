ea873a89b7804f7575897a623d52e91ad1fb124c
Gowtham Garimella <ggarime1@jhu.edu>
2018-07-18 18:55:45 -0400

    Add jacobians

--- controllers/abstract_controller.py
+++ controllers/abstract_controller.py
@@ -2,43 +2,51 @@
 
 from abc import ABCMeta, abstractmethod
 from optimal_control_framework.dynamics import AbstractDynamicSystem
+import numpy as np
+
 
 class AbstractController(object):
-  __metaclass__ = ABCMeta
+    __metaclass__ = ABCMeta
 
-  def __init__(self, dynamics, discrete=True):
-    """
-    Constructor that stores dynamics
-    Parameters:
-    dynamics -- Should be a subclass of
-                AbstractDynamicSystem
-    discrete -- If the controller is discrete or continous
-    """
-    self.dynamics = dynamics
-    self.discrete = discrete
-    self.m = self.dynamics.m
-    assert(isinstance(dynamics, AbstractDynamicSystem))
+    def __init__(self, dynamics, discrete=True):
+        """
+        Constructor that stores dynamics
+        Parameters:
+        dynamics -- Should be a subclass of
+                    AbstractDynamicSystem
+        discrete -- If the controller is discrete or continous
+        """
+        self.dynamics = dynamics
+        self.discrete = discrete
+        self.m = self.dynamics.m
+        assert(isinstance(dynamics, AbstractDynamicSystem))
 
-  def control(self, i, x):
-    """
-    Compute the control to be sent based on inputs and
-    a noise sample (sampled internally)
-    Parameters:
-    i   -- Index along trajectory for discrete controllers
-           It can also be  time along trajectory for continous
-           controllers
-    x   -- Current state
-    """
-    return self.deterministic_control(i,x)
+    def jacobian(self, i, x):
+        """
+        Compute the jacobian of the controller w.r.t x at a specified time/index
+        """
+        return np.zeros(self.dynamics.m, self.dynamics.n)
 
-  @abstractmethod
-  def deterministic_control(self, i, x):
-    """
-    Compute a deterministic control purely based on inputs
-    Parameters:
-    i   -- Index along trajectory for discrete controllers
-           It can also be  time along trajectory for continous
-           controllers
-    x   -- Current state
-    """
-    pass
+    def control(self, i, x):
+        """
+        Compute the control to be sent based on inputs and
+        a noise sample (sampled internally)
+        Parameters:
+        i   -- Index along trajectory for discrete controllers
+               It can also be  time along trajectory for continous
+               controllers
+        x   -- Current state
+        """
+        return self.deterministic_control(i, x)
+
+    @abstractmethod
+    def deterministic_control(self, i, x):
+        """
+        Compute a deterministic control purely based on inputs
+        Parameters:
+        i   -- Index along trajectory for discrete controllers
+               It can also be  time along trajectory for continous
+               controllers
+        x   -- Current state
+        """
+        pass
--- dynamics/unicycle_dynamics.py
+++ dynamics/unicycle_dynamics.py
@@ -2,10 +2,12 @@
 from optimal_control_framework.dynamics import AbstractDynamicSystem
 import numpy as np
 
+
 class UnicycleDynamics(AbstractDynamicSystem):
     """
     Zero order dynamics with n = m = 1
     """
+
     def __init__(self):
         """
         Constructor that initializes the order
@@ -17,24 +19,23 @@ class UnicycleDynamics(AbstractDynamicSystem):
         """
         Basic dynamics of xdot = u + w
         """
-        v= u[0]
+        v = u[0]
         thetadot = u[1]
         theta = x[2]
-        return np.array([v*np.cos(theta), v*np.sin(theta), thetadot]) + w
+        return np.array([v * np.cos(theta), v * np.sin(theta), thetadot]) + w
 
     def jacobian(self, t, x, u, w):
         """
         Find jacobian of xdot wrt x, u, w
         """
-        v= u[0]
+        v = u[0]
         theta = x[2]
+        c_theta = np.cos(theta)
+        s_theta = np.sin(theta)
         fx = np.array([[0, 0, 0],
                        [0, 0, 0],
-                       [-v*np.sin(theta), v*np.cos(theta), 0]])
-        fu = np.array([[np.cos(theta), np.sin(theta), 0],
-                       [0, 0, 1]])
+                       [-v * s_theta, v * c_theta, 0]]).T
+        fu = np.array([[c_theta, s_theta, 0],
+                       [0, 0, 1]]).T
         fw = np.eye(3)
         return [fx, fu, fw]
-
-
-
